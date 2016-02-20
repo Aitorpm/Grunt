365e9f95ea8a0b0369343278e21f8247da631c5b
Aitor Piñeyro <itor95@gmail.com>
2016-02-20 14:43:32 +0100

    1a Version

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+//Contenedor
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        //uglify
+        uglify: {
+            options: {
+                mangle: false,
+                compress: {
+                    drop_console: true
+                }
+            },
+            js: {
+                files: [{
+                    cwd: 'js/src/',
+                    expand: true,
+                    src: '*.js',
+                    dest: 'js/min/'
+                }]
+            }
+        }
+    });
+
+    // loadNpmTasks
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Run Default task(s).
+    grunt.registerTask('default', ['uglify']);
+};
